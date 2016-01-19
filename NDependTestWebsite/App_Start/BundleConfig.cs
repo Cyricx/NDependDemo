a2490e296e4bc485ab18e52e7b3b8c465311225a
Cyricx <contact@chrisbyram.com>
2016-01-18 19:25:11 -0600

    Locked down register bundles method

--- NDependTestWebsite/App_Start/BundleConfig.cs
+++ NDependTestWebsite/App_Start/BundleConfig.cs
@@ -6,7 +6,7 @@ namespace NDependTestWebsite
     public class BundleConfig
     {
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
-        public static void RegisterBundles(BundleCollection bundles)
+        internal static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
